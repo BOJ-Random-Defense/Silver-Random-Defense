fb6b8a4eac6f6345fab9f77e53f5fe22f4324fe2
I-kotori <butterflyjin77@gmail.com>
2025-01-23 23:45:37 +0900

    edits in Problem B

--- 1st/(S2)b-17827.cpp
+++ 1st/(S2)b-17827.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int N, M, V;
 int arr[202020];
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); 
-    cout.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); 
+    cout.tie(nullptr);
     cin >> N >> M >> V;
     for(int i=0; i<N; i++){
         cin >> arr[i];
